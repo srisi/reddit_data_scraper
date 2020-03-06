45086dc05e41eb1046620d55358a9d5c9ba26ddf
Stephan Risi <stephan.risi@gmail.com>
2020-03-05 23:03:23 -0500

    Finished initial scraper implementation

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timezone
-import urllib
-from IPython import embed
+import urllib.request
+import html
 import json
+import csv
+from pathlib import Path
 
 
 class RedditScraper:
@@ -21,6 +23,8 @@ class RedditScraper:
         :param min_score: minimum score (upvotes) for a comment to be included.
         """
 
+        # the code in the init file mostly just validates the input, e.g. are the submitted dates
+        # formed correctly
         for param in [search_term, subreddit]:
             if not (isinstance(param, str) or param is None):
                 raise ValueError("Search term and sub reddit have to be strings.")
@@ -44,14 +48,79 @@ class RedditScraper:
         self.number_of_results = number_of_results
         self.min_score = min_score
 
-    def execute_query_and_store_as_csv(self):
+    @property
+    def filename(self):
         """
-        Execute search and stores result
+        generate filename for this search
+        :return:
+        """
+
+        name_parts = []
+        if self.search_term:
+            name_parts.append(self.search_term)
+        if self.subreddit:
+            name_parts.append(f'r{self.subreddit}')
+        if self.min_score > 0:
+            name_parts.append(f'minscore_{self.min_score}')
+
+        if self.start_date != '1990-01-01' or self.end_date != '2030-01-01':
+            name_parts.append(f'{self.start_date}to{self.end_date}')
+
+        return "_".join(name_parts)
+
+
+    def execute_query_and_store_as_csv(self, output_filename=None):
+        """
+        Execute search and stores result as a csv file
 
         :return:
         """
 
-        url = self.generate_query()
+        url = self._generate_query_url()
+        documents = self._get_documents(url)
+        self._store_documents_to_csv(documents, output_filename)
+
+    def _generate_query_url(self):
+        """
+        Generates a url to query pushshift.io with the passed parameters
+        :return: str
+
+        >>> r = RedditScraper(search_term='fentanyl', subreddit='boston')
+        >>> r._generate_query_url()
+        'https://api.pushshift.io/reddit/search/?q=fentanyl&subreddit=boston&size=100'
+
+        >>> r2 = RedditScraper(start_date='2014-01-01', end_date='2014-12-31')
+        >>> r2._generate_query_url()
+        'https://api.pushshift.io/reddit/search/?size=100&after=1388552400&before=1420002000'
+        """
+
+        search_params = {}
+        if self.search_term:
+            search_params['q'] = self.search_term
+        if self.subreddit:
+            search_params['subreddit'] = self.subreddit
+        search_params['size'] = self.number_of_results
+
+        if self.start_date != '1990-01-01':
+            search_params['after'] = self.start_date_timestamp
+        if self.end_date != '2030-01-01':
+            search_params['before'] = self.end_date_timestamp
+
+        url = f'https://api.pushshift.io/reddit/search/?{urllib.parse.urlencode(search_params)}'
+
+        if self.min_score and self.min_score > 0:
+            url += f'&score=>{self.min_score}'
+
+        return url
+
+    def _get_documents(self, url):
+        """
+        Downloads up to number_of_documents matching the search query from pushshift.io
+
+        :param url: str
+        :return: list[dict]
+        """
+
         documents = []
         with urllib.request.urlopen(url) as response:
             response = response.read().decode('utf-8')
@@ -74,36 +143,33 @@ class RedditScraper:
                     'subreddit': doc_raw['subreddit'],
                     'score': doc_raw['score'],
                     'url': url,
-                    'text': doc_raw['body'],
+                    'text': html.unescape(doc_raw['body']),
                 })
+        return documents
 
-
-        embed()
-
-
-    def generate_query(self):
+    def _store_documents_to_csv(self, documents, filename):
         """
-        This function generates a query to pushshift.io with the passed parameters
+        Stores the downloaded documents in a csv in the data folder.
+        If no filename is provided, it will automatically generate one.
+
+        :param documents: list[dict]
+        :param filename: str
         :return:
         """
 
-        search_params = {}
-        if self.search_term:
-            search_params['q'] = self.search_term
-        if self.subreddit:
-            search_params['subreddit'] = self.subreddit
-        search_params['size'] = self.number_of_results
-        search_params['after'] = self.start_date_timestamp
-        search_params['before'] = self.end_date_timestamp
+        if not filename:
+            filename = self.filename
 
-        url = f'https://api.pushshift.io/reddit/search/?{urllib.parse.urlencode(search_params)}'
+        with open(Path('data', f'{filename}.csv'), 'w', encoding='utf-8') as csvfile:
+            fieldnames = ['date', 'author', 'subreddit', 'score', 'url', 'text']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for doc in documents:
+                writer.writerow(doc)
 
-        if self.min_score and self.min_score > 0:
-            url += f'&score=>{self.min_score}'
-
-        return url
 
 if __name__ == '__main__':
-    r = RedditScraper(search_term='fentanyl', subreddit='Boston', min_score=5)
+    r = RedditScraper(search_term='fentanyl', number_of_results=2000, min_score=5,
+                      start_date='2016-01-01')
     r.execute_query_and_store_as_csv()
 
